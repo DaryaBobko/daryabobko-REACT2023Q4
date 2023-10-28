5bf20454a99c6e5395561fa2b80b1bed049b5c4b
Darya <bobko.dv@gmail.com>
2023-10-28 22:45:57 +0200

    refactor: updated app architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import ListItem from './components/ListItem/ListItem';
 import SearchPanel from './components/SearchPanel/SearchPanel';
 import SearchButton from './components/SearchButton/SearchButton';
@@ -5,55 +7,57 @@ import SearchButton from './components/SearchButton/SearchButton';
 import './App.scss';
 import './styles/space.scss';
 
-function App() {
-  return (
-    <>
-      <div className="container header">
-        <SearchPanel />
-        <div className="mh-4">
-          <SearchButton />
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container header">
+          <SearchPanel />
+          <div className="mh-4">
+            <SearchButton />
+          </div>
         </div>
-      </div>
-      <div className="content pv-4">
-        <div className="container">
-          <div className="source-header">Results: </div>
-          {/* <div className="source-no-results">Nothing found </div> */}
-          <ul>
-            <ListItem
-              title="Lorem ipsum"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <div className="content pv-4">
+          <div className="container">
+            <div className="source-header">Results: </div>
+            {/* <div className="source-no-results">Nothing found </div> */}
+            <ul>
+              <ListItem
+                title="Lorem ipsum"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
               in reprehenderit in voluptate velit esse cillum dolore eu fugiat
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
-            <ListItem
-              title="Lorem ipsum"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              />
+              <ListItem
+                title="Lorem ipsum"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
               in reprehenderit in voluptate velit esse cillum dolore eu fugiat
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
-            <ListItem
-              title="Lorem ipsum"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              />
+              <ListItem
+                title="Lorem ipsum"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
               in reprehenderit in voluptate velit esse cillum dolore eu fugiat
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
-          </ul>
+              />
+            </ul>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
