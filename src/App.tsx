1f9e64a6187563aee2e0f184f0fb5538321f8831
Darya <bobko.dv@gmail.com>
2023-11-05 20:38:18 +0200

    feat: conversion of class components to functional components with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,39 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
+import { ANIMAL_SEARCH_VALUE } from './constants';
+import { Animal, AnimalSearchResult } from './models/AnimalSearchResult';
+
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import CustomError from './components/CustomError/CustomError';
 import ListItem from './components/ListItem/ListItem';
 import Input from './components/Input/Input';
 import Button from './components/Button/Button';
 
-import './App.scss';
+import styles from './App.module.scss';
 import './styles/space.scss';
-import { Animal, AnimalSearchResult } from './models/AnimalSearchResult';
-import { ANIMAL_SEARCH_VALUE } from './constants';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-import CustomError from './components/CustomError/CustomError';
 
-type AppState = {
-  animalsSearchResult: AnimalSearchResult | null;
-  value: string;
-  filteredAnimals: Animal[];
-  isShowError: boolean;
-};
+const App: React.FC = () => {
+  const [filteredAnimals, setFilteredAnimals] = useState([] as Animal[]);
+  const [value, setValue] = useState(
+    localStorage.getItem(ANIMAL_SEARCH_VALUE) || ''
+  );
+  const [isShowError, setShowError] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
-class App extends Component<unknown, AppState> {
-  public state: AppState = {
-    animalsSearchResult: null,
-    value: localStorage.getItem(ANIMAL_SEARCH_VALUE) || '',
-    filteredAnimals: [],
-    isShowError: false,
+  const showError = () => {
+    setShowError((prevState) => !prevState);
   };
 
-  onSearchChange = (value: string) => {
-    this.setState({ value: value });
+  const onSearchChange = (value: string) => {
+    setValue(value);
   };
 
-  showFilteredItem = () => {
-    localStorage.setItem(ANIMAL_SEARCH_VALUE, this.state.value);
-
-    this.setState({
-      filteredAnimals: this.search(
-        this.state.animalsSearchResult?.animals ?? [],
-        this.state.value
-      ),
-    });
-  };
-
-  componentDidMount = () => {
-    fetch('https://stapi.co/api/v1/rest/animal/search')
-      .then((response) => response.json())
-      .then((animalsSearchResult: AnimalSearchResult) => {
-        this.setState({
-          animalsSearchResult,
-          filteredAnimals: this.search(
-            animalsSearchResult.animals,
-            this.state.value
-          ),
-        });
-      });
-  };
-
-  search = (animals: Animal[], value: string): Animal[] => {
+  const search = (animals: Animal[], value: string): Animal[] => {
     if (value.length === 0) {
       return animals;
     }
+
     if (animals) {
       return animals.filter((animal) => {
         return animal.name.toLowerCase().includes(value.toLowerCase());
@@ -68,42 +43,53 @@ class App extends Component<unknown, AppState> {
     return [];
   };
 
-  showError = () => {
-    this.setState({ isShowError: !this.state.isShowError });
+  const showFilteredItem = () => {
+    localStorage.setItem(ANIMAL_SEARCH_VALUE, value);
+    getAnimals();
   };
 
-  render() {
-    return (
-      <ErrorBoundary>
-        <div className="container header">
-          <div className="search-panel">
-            <Input
-              onSearchChange={this.onSearchChange}
-              value={this.state.value}
-            />
-            <Button onClick={this.showFilteredItem}>Search</Button>
-          </div>
-          <Button onClick={this.showError}>Throw error</Button>
+  useEffect(() => {
+    getAnimals();
+  });
+
+  const getAnimals = () => {
+    fetch('https://stapi.co/api/v1/rest/animal/search')
+      .then((response) => response.json())
+      .then((animalsSearchResult: AnimalSearchResult) => {
+        setFilteredAnimals(search(animalsSearchResult.animals, value));
+      })
+      .catch((error) => {
+        console.warn(error);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  return (
+    <ErrorBoundary>
+      <div className={classNames(styles.container, styles.header)}>
+        <div className={styles.searchPanel}>
+          <Input onSearchChange={onSearchChange} value={value} />
+          <Button onClick={showFilteredItem}>Search</Button>
         </div>
-        <div className="content pv-4">
-          <div className="container">
-            <ul className="list-wrapper">
-              {this.state.filteredAnimals.map((animal: Animal) => (
-                <ListItem
-                  key={animal.uid}
-                  uid={animal.uid}
-                  name={animal.name}
-                  earthAnimal={animal.earthAnimal}
-                />
+        <Button onClick={showError}>Throw error</Button>
+      </div>
+      <div className={classNames(styles.content, 'pv-4')}>
+        <div className={styles.container}>
+          {isLoading ? (
+            <div className={styles.spinner} />
+          ) : (
+            <ul className={styles.listWrapper}>
+              {filteredAnimals.map((animal: Animal) => (
+                <ListItem key={animal.uid} {...animal} />
               ))}
             </ul>
-          </div>
+          )}
         </div>
+      </div>
 
-        {this.state.isShowError && <CustomError />}
-      </ErrorBoundary>
-    );
-  }
-}
+      {isShowError && <CustomError />}
+    </ErrorBoundary>
+  );
+};
 
 export default App;
